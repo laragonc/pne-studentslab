ba78d1ea0eda15857302b4ed9e5fca32130d1fd9
laragonc <l.gonzalezc.2023@alumnos.urjc.es>
2024-01-31 18:17:36 +0100

    This is my first modification

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,15 +1,8 @@
-def read_data_from_file(filename):
-    with open(filename, "r") as f:
-        result = []
-        header = next(f).split(",")
-        header[-1] = header[-1].replace("\n","")
-        for i in range(0, len(header)):
-            result.append([])
-        for line in f:
-            components = line.strip("\n").split(",")
-            if len(components) == len(header):
-                for i in range(0, len(components)):
-                    result[i].append(components[i])
-    return header, result
-sequence = read_data_from_file("dna")
-
+with open("dna", "r") as f:
+    count = {"A": 0, "G": 0, "C": 0, "T": 0}
+    for i in f:
+        for base in i:
+            if base in count:
+                count[base] += 1
+    for k, v in count.items():
+        print(str(k) + ":", v)
