a2bd9b6fd59256ac8249b85ffae269133e346b00
laragonc <l.gonzalezc.2023@alumnos.urjc.es>
2024-02-28 17:47:37 +0100

    This is my first modification

---
+++ P02/Client0.py
@@ -0,0 +1,14 @@
+import socket
+class Client:
+    def __init__(self, IP, PORT):
+        self.IP = IP
+        self.PORT = PORT
+    def __str__(self):
+        return "Connection to SERVER at" +, PORT: {port}"
+
+    def ping(self):
+        print("OK")
+    #def talk(self, msg):
+
+
+
