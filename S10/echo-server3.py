836d9373e1f519e948367b2263689634e3f6f1f0
laragonc <l.gonzalezc.2023@alumnos.urjc.es>
2024-03-05 16:43:43 +0100

    This is my first modification

---
+++ S10/echo-server3.py
@@ -0,0 +1,34 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8081
+IP = "212.128.255.90" # this IP address is local, so only requests from the same machine are possible
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+list_client = []
+count = 1
+flag = True
+while flag:
+    (rs, address) = ls.accept()# returns total socket and the adress
+    msg = rs.recv(2048).decode("utf-8") #readed from client and tranform to utf
+    print("CONECTION", count, "Client IP, PORT: ", address)
+    newMsg = "ECHO: " + msg
+    rs.send(newMsg.encode()) #encode to bites and then send it
+    count += 1
+    list_client.append(address)
+    if count == 6:
+        flag = False
+        for i in list_client:
+            print("Client", ":", i)
+        print()
+    rs.close()
+ls.close()
---
+++ S10/theory.py
@@ -0,0 +1,25 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8081
+IP = "212.128.255.90" # this IP address is local, so only requests from the same machine are possible
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+while True:
+    (rs, address) = ls.accept()# returns total socket and the adress
+    print(f"Client {address}")
+    msg = rs.recv(2048).decode("utf-8") #readed from client and tranform to utf
+    print("The client says..." + msg)
+    newMsg = "I'm a happy server"
+    rs.send(newMsg.encode()) #encode to bites and then send it
+    rs.close()
+ls.close()
