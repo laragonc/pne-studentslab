bd0db1c4955a0ca96e4605c83b373fe2d6b0d74a
laragonc <l.gonzalezc.2023@alumnos.urjc.es>
2024-04-25 17:37:03 +0200

    This is my first modification

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -13,11 +13,7 @@ def read_html_file(filename):
     contents = Path("html/" + filename).read_text()
     contents = j.Template(contents)
     return contents
-def read_fasta(filename):
-    first_line = Path(filename).read_text().find("\n")
-    seq = Path(filename).read_text()[first_line:]
-    seq = seq.replace("\n", "")
-    return seq
+
 
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
@@ -29,25 +25,21 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """This method is called whenever the client invokes the GET method
         in the HTTP protocol request"""
-
+        url_path = urlparse(self.path)
+        path = url_path.path  # we get it from here
+        arguments = parse_qs(url_path.query)
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
         # Open the form1.html file
         # Read the index from the file
-        if self.path == "/" or self.path.startswith("/echo"):
+        if path == "/" or path.startswith("/echo"):
             contents = Path("./html/index.html").read_text()
             self.send_response(200)
-            url_path = urlparse(self.path)
-            path = url_path.path # we get it from here
-            arguments = parse_qs(url_path.query)
-            ping = arguments.get("ping", [""])[0]
-            if ping:
-                contents = Path("./html/ping.html").read_text()
-                self.send_response(200)
-            else:
-                contents = Path("./html/index.html").read_text()
-                self.send_response(200)
+
+        elif path == "/ping":
+            contents = Path("./html/ping.html").read_text()
+            self.send_response(200)
         else:
             contents = Path("./html/error.html").read_text()
             self.send_response(404)
