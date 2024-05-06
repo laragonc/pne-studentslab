d2c188adf004e956063ce923c2bc75754fad226f
laragonc <l.gonzalezc.2023@alumnos.urjc.es>
2024-05-06 18:39:03 +0200

    This is my first modification

--- P02/e5.py
+++ P02/e5.py
@@ -4,18 +4,31 @@ genes = ["U5", "ADA", "FRAT1"]
 genes_dir = "../sequences/"
 
 PRACTICE = 2
-EXERCISE = 4
+EXERCISE = 5
 
 IP = "172.20.10.3"
 PORT = 8081
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 def get_file_path(gene):
     return genes_dir + gene
 def req_response_from_server(client, msg):
     print("To Sever: {}".format(msg), sep="")
     response = client.talk(msg)
     print(f"From Server:{response}")
-print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
-
+def create_fragment(sequence):
+    fragment = ""
+    count = 0
+    fragment_list = []
+    for i in sequence:
+        count += 1
+        fragment += 1
+        if count == 10:
+            fragment_list.append(fragment)
+            fragment = ""
+            count = 0
+    if fragment:
+        fragment_list.append(fragment)
+    return fragment_list
 c = Client(IP, PORT)
 for g in genes:
     s = Seq()
